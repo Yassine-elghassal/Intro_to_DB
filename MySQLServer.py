c44229bbcde23f6bf5d8b29461e21f123aac0c08
Yassine-elghassal <yassine.elghassal0@gmail.com>
2025-02-16 16:27:30 +0100

    first commit

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,10 +14,10 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
             print("Database 'alx_book_store' created successfully!")
-    
-    except Error as e:
+
+    except mysql.connector.Error as e:  # Updated exception handling
         print(f"Error: {e}")
-    
+
     finally:
         if 'cursor' in locals():
             cursor.close()
